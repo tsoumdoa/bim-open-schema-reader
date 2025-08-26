d7d07948b4e95ad93a52c326082f9cc1c47e7bca
tsoumdoa <8blondes.swerves@icloud.com>
2025-08-26 20:47:31 +0800

    minor err fix

--- src/app/components/analytical-display.tsx
+++ src/app/components/analytical-display.tsx
@@ -3,92 +3,92 @@ import { useDuckDB } from "../hooks/use-duckdb";
 import { ParquetBlob } from "../utils/types";
 import * as duckdb from "@duckdb/duckdb-wasm";
 import {
-  runQuery,
-  importParquetFromBuffer,
-  listTables,
+	runQuery,
+	importParquetFromBuffer,
+	listTables,
 } from "../utils/duckdb-wasm-helpers";
 
 async function runTest(
-  db: duckdb.AsyncDuckDB,
-  c: duckdb.AsyncDuckDBConnection,
-  setRows: (rows: any[]) => void
+	db: duckdb.AsyncDuckDB,
+	c: duckdb.AsyncDuckDBConnection,
+	setRows: (rows: (string | number)[][]) => void
 ) {
-  const jsonRowContent = [
-    { col1: 1, col2: "foo" },
-    { col1: 2, col2: "bar" },
-  ];
-  await db.registerFileText("rows.json", JSON.stringify(jsonRowContent));
-  await c.insertJSONFromPath("rows.json", { name: "rows" });
+	const jsonRowContent = [
+		{ col1: 1, col2: "foo" },
+		{ col1: 2, col2: "bar" },
+	];
+	await db.registerFileText("rows.json", JSON.stringify(jsonRowContent));
+	await c.insertJSONFromPath("rows.json", { name: "rows" });
 
-  //query
-  const query = `SELECT * FROM rows`;
-  const { headers, rows } = await runQuery(c, query);
-  setRows(rows);
+	//query
+	const query = `SELECT * FROM rows`;
+	const { headers, rows } = await runQuery(c, query);
+	setRows(rows);
 
-  console.log("Headers:", headers);
-  console.log("Rows:", rows);
+	console.log("Headers:", headers);
+	console.log("Rows:", rows);
 }
 
 function DbDisplay(props: {
-  db: duckdb.AsyncDuckDB;
-  c: duckdb.AsyncDuckDBConnection;
-  parquetFileEntries: ParquetBlob[];
+	db: duckdb.AsyncDuckDB;
+	c: duckdb.AsyncDuckDBConnection;
+	parquetFileEntries: ParquetBlob[];
 }) {
-  const { db, c } = props;
-  const hasRun = useRef(false); // kidna have to do this due to react strict mode
+	const { db, c } = props;
+	const hasRun = useRef(false); // kidna have to do this due to react strict mode
 
-  const [rows, setRows] = useState<any[]>([]);
+	const [rows, setRows] = useState<(string | number)[][]>([]);
 
-  useEffect(() => {
-    if (!hasRun.current) {
-      runTest(db, c, setRows);
+	useEffect(() => {
+		if (!hasRun.current) {
+			runTest(db, c, setRows);
 
-      props.parquetFileEntries.forEach(async (entry) => {
-        const { filename, parquet } = entry;
-        await importParquetFromBuffer(
-          db,
-          c,
-          parquet,
-          filename.replace(".parquet", ""),
-          filename
-        );
-        await listTables(c);
-      });
+			props.parquetFileEntries.forEach(async (entry) => {
+				const { filename, parquet } = entry;
+				await importParquetFromBuffer(
+					db,
+					c,
+					parquet,
+					filename.replace(".parquet", ""),
+					filename
+				);
+				await listTables(c);
+			});
 
-      hasRun.current = true;
-    }
-  }, [db, c]);
+			hasRun.current = true;
+		}
+	}, [db, c, props.parquetFileEntries]);
 
-  return (
-    <div>
-      DB Display
-      <div>
-        {rows.map((row, i) => (
-          <div key={i}>{row.join(", ")}</div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			DB Display
+			<div>
+				{rows.map((row, i) => (
+					<div key={i}>{row.join(", ")}</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default function AnalyticalDisplay(props: {
-  parquetFileEntries: ParquetBlob[];
+	parquetFileEntries: ParquetBlob[];
 }) {
-  const { dbRef, connectionRef, error, loading } = useDuckDB();
+	const { dbRef, connectionRef, error, loading } = useDuckDB();
 
-  if (!error && loading) {
-    return <div>Initializing...</div>;
-  }
+	if (!error && loading) {
+		return <div>Initializing...</div>;
+	}
 
-  if (dbRef.current && connectionRef.current) {
-    return (
-      <DbDisplay
-        db={dbRef.current}
-        c={connectionRef.current}
-        parquetFileEntries={props.parquetFileEntries}
-      />
-    );
-  }
+	if (dbRef.current && connectionRef.current) {
+		return (
+			<DbDisplay
+				db={dbRef.current}
+				c={connectionRef.current}
+				parquetFileEntries={props.parquetFileEntries}
+			/>
+		);
+	}
 
-  return <div>Something went wrong, try again</div>;
+	return <div>Something went wrong, try again</div>;
 }
