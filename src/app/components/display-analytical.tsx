b07e6fce4227627d7307783182e5b5dcaab57629
tsoumdoa <8blondes.swerves@icloud.com>
2025-09-02 21:13:04 +0800

    context dropdown menu added

--- src/app/components/display-analytical.tsx
+++ src/app/components/display-analytical.tsx
@@ -1,5 +1,5 @@
 import { useDuckDB } from "../hooks/use-duckdb";
-import { ParquetBlob, QueryObjects } from "../utils/types";
+import { ParquetBlob, QueryObject, QueryObjects } from "../utils/types";
 import * as duckdb from "@duckdb/duckdb-wasm";
 import { useImportParquet } from "../hooks/use-import-parquet";
 import { SimpleErrMessage } from "./simple-err-message";
@@ -12,12 +12,54 @@ import SideBarContent from "./side-bar-content";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AddQuery } from "./add-query-button";
 import { useQueryObjects } from "../hooks/use-query-objects";
-import { Separator } from "@/components/ui/separator";
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipTrigger,
-} from "@/components/ui/tooltip";
+import QueryDisplayItem from "./query-display-item";
+
+function DashboardHeader(props: {
+	db: duckdb.AsyncDuckDB;
+	c: duckdb.AsyncDuckDBConnection;
+	fileName: string;
+}) {
+	return (
+		<div className="flex flex-row items-center justify-start  gap-x-1">
+			<SidebarTrigger className="" />
+			<p className="text-sm py-2">
+				file name: <span className="font-bold">{props.fileName}</span>
+			</p>
+		</div>
+	);
+}
+
+function DashboardMain(props: {
+	queryObjects: QueryObjects;
+	addQuery: (queryObject: QueryObject) => void;
+	removeQuery: (queryObject: QueryObject) => void;
+}) {
+	return (
+		<div className="flex flex-col items-start gap-y-2 ">
+			{props.queryObjects.length > 0 &&
+				props.queryObjects.map((q, i) => {
+					return (
+						<QueryDisplayItem key={`${i}-${q.queryTile}`} queryObject={q} />
+					);
+				})}
+			<AddQuery addQuery={props.addQuery} />
+		</div>
+	);
+}
+
+function SideBar(props: {
+	db: duckdb.AsyncDuckDB;
+	c: duckdb.AsyncDuckDBConnection;
+	fileName: string;
+}) {
+	return (
+		<Sidebar className="pt-11 overflow-y-scroll h-full ">
+			<div className="p-2">
+				<SideBarContent duckDbConnection={props.c} />
+			</div>
+		</Sidebar>
+	);
+}
 
 function DashboardContainer(props: {
 	db: duckdb.AsyncDuckDB;
@@ -27,41 +69,14 @@ function DashboardContainer(props: {
 	const { queryObjects, addQuery, removeQuery } = useQueryObjects();
 	return (
 		<SidebarProvider>
-			<Sidebar className="pt-11 overflow-y-scroll h-full ">
-				<div className="p-2">
-					<SideBarContent duckDbConnection={props.c} />
-				</div>
-			</Sidebar>
-
+			<SideBar db={props.db} c={props.c} fileName={props.fileName} />
 			<main>
-				<div className="flex flex-row items-center justify-start  gap-x-1">
-					<SidebarTrigger className="" />
-					<p className="text-sm py-2">
-						file name: <span className="font-bold">{props.fileName}</span>
-					</p>
-				</div>
-				<div className="flex flex-col items-start gap-y-2 ">
-					{queryObjects.length > 0 &&
-						queryObjects.map((q, i) => {
-							return (
-								<div key={`${i}-${q.queryTile}`}>
-									<Tooltip delayDuration={150}>
-										<TooltipTrigger asChild>
-											<span className="font-bold text-md leading-tight hover:cursor-help">
-												{`${q.queryTile} `}{" "}
-											</span>
-										</TooltipTrigger>
-										<TooltipContent side="top" align="start">
-											<p>{q.explaination}</p>
-										</TooltipContent>
-									</Tooltip>
-									<span className="text-xs text-neutral-500">{`<${q.queryCategory || "n/a"}>`}</span>
-									<Separator className="my-4" />
-								</div>
-							);
-						})}
-					<AddQuery addQuery={addQuery} />
-				</div>
+				<DashboardHeader db={props.db} c={props.c} fileName={props.fileName} />
+				<DashboardMain
+					queryObjects={queryObjects}
+					addQuery={addQuery}
+					removeQuery={removeQuery}
+				/>
 			</main>
 		</SidebarProvider>
 	);
