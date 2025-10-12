8d24f188738fb94fa43239e767812a23e6d57712
tsoumdoa <8blondes.swerves@icloud.com>
2025-10-12 22:56:46 +0800

    enhaced room and level query!

--- src/app/sql/room-count-by-level.ts
+++ src/app/sql/room-count-by-level.ts
@@ -1,6 +1,6 @@
 import { sql } from "../utils/queries";
 
-export const simpleRoomSchedule = sql`
+export const roomScheduleByLevel = sql`
   WITH
     double_data AS (
       SELECT
@@ -14,7 +14,6 @@ export const simpleRoomSchedule = sql`
     ),
     pivot_double_data AS (
       PIVOT double_data ON name_1 IN ("Volume", "Area", "Unbounded Height", "Perimeter") USING first (VALUE),
-      -- first (Units) AS param_units
       GROUP BY
         LocalId,
         name
@@ -46,19 +45,26 @@ export const simpleRoomSchedule = sql`
     joint AS (
       SELECT DISTINCT
         psd.*,
-        pdd.Area * 0.092903 AS area_m2,
+        (pdd.Area * 0.092903) AS area_m2,
         pdd.Perimeter * 304.8 AS perimeter_mm,
-        pdd."Unbounded Height" * 304.8 AS height_mm,
-        pdd.Volume * 0.0283168 volume_m3,
+        (pdd."Unbounded Height" * 304.8) AS height_mm,
+        (pdd.Volume * 0.0283168) AS volume_m3
       FROM
         pivot_str_data AS psd
         LEFT JOIN pivot_double_data AS pdd ON psd.LocalId = pdd.LocalId
     )
   SELECT
-    *
+    COALESCE(Level, 'Unplaced') AS Level,
+    COUNT(*) AS room_count,
+    SUM(area_m2) AS total_area_m2,
+    sum(perimeter_mm) AS total_perimeter_mm,
+    MIN(height_mm) AS min_height_mm,
+    MAX(height_mm) AS max_height_mm,
+    SUM(volume_m3) AS total_volume_m3
   FROM
     joint
+  GROUP BY
+    Level
   ORDER BY
-    Level,
-    name;
+    Level;
 `;
