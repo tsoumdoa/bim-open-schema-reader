d63430a64e6450ac1c36346bcc32b0a78402b1fd
tsoumdoa <8blondes.swerves@icloud.com>
2026-03-31 21:36:49 +0800

    next bs allowDevOriign

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactCompiler: true,
+	allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev", "192.168.1.56"],
 };
 
 export default nextConfig;
